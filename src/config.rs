1e5edc4d69824360900bf193bb57aae40e67dc8a
12zhan <88201006+12zhan@users.noreply.github.com>
2025-10-28 17:28:01 +0800

    Update config.rs

--- src/config.rs
+++ src/config.rs
@@ -105,7 +105,7 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &str = "v+iUtH7YUrETIJJrgJZyLDgMh7Oc5HrtAPBpNJi5Vt8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
